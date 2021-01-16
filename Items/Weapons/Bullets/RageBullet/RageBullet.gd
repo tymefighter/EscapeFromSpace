80f4f21013abcc06902ab8661410febb8880e0fd
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-01-16 22:05:37 +0530

    Add weapons and scripts

---
+++ Items/Weapons/Bullets/RageBullet/RageBullet.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var damage = 10
+var velocity = Vector2.ZERO
+
+func _ready():
+	pass
+	
+func _process(delta):
+	position += velocity * delta
+
+func _on_Area2D_body_entered(body):
+	
+	if body.get("health"):
+		body.health -= damage
+	
+	queue_free()
